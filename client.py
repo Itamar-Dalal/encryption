4c9abc25c6936fa1e6a746e09e33acc80a2bbce9
Itamar-Dalal <dalalitamar@gmail.com>
2024-02-05 22:41:52 +0200

    working client

---
+++ client.py
@@ -0,0 +1,231 @@
+import tkinter as tk
+from tkinter import font
+import tcp_by_size
+from socket import socket, AF_INET, SOCK_STREAM
+from tcp_by_size import send_with_size, recv_by_size
+from sys import argv
+
+
+IP = "127.0.0.1"
+PORT = 1234
+
+class Client(tk.Tk):
+    def __init__(self, ip, port, width=350, height=250):
+        super().__init__()
+        self.width = width
+        self.height = height
+        self.ip = ip
+        self.port = port
+        self.cli_sock = socket(AF_INET, SOCK_STREAM)
+
+    def init_home(self):
+        self.title("User Login")
+        self.geometry(f"{self.width}x{self.height}")
+        self.configure(bg="#1E2533")
+        header_font = font.Font(family="Helvetica", size=16, weight="bold")
+        header_label = tk.Label(
+            self, text="User Login", font=header_font, bg="#1E2533", fg="#E0E6F0"
+        )
+        header_label.pack(pady=20, padx=10, ipadx=10, ipady=5)
+        self.button_style = {
+            "bg": "#46516e",
+            "fg": "white",
+            "borderwidth": 0,
+            "activebackground": "#2980B9",
+            "activeforeground": "white",
+        }
+        register_button = tk.Button(
+            self,
+            text="Open Register Window",
+            command=self.init_register,
+            **self.button_style,
+        )
+        register_button.pack(pady=20, padx=10, ipadx=10, ipady=5)
+        login_button = tk.Button(
+            self, text="Open Login Window", command=self.init_login, **self.button_style
+        )
+        login_button.pack(pady=20, padx=10, ipadx=10, ipady=5)
+        self.mainloop()
+
+    def init_register(self):
+        self.register_window = tk.Toplevel(self)
+        self.register_window.title("Register")
+        self.register_window.geometry(f"300x440")
+        self.register_window.configure(bg="#1E2533")
+        header_font = font.Font(family="Helvetica", size=14, weight="bold")
+        tk.Label(
+            self.register_window,
+            text="Register",
+            font=header_font,
+            bg="#1E2533",
+            fg="#E0E6F0",
+        ).pack(pady=20, padx=10, ipadx=10, ipady=5)
+        labels = ["Username:", "Email Address:", "Password:", "Verify password:"]
+        self.register_entries = [
+            tk.Entry(
+                self.register_window,
+                bg="#46516e",
+                fg="#E0E6F0",
+                show="*" if label in ("Password:", "Verify password:") else None,
+            )
+            for label in labels
+        ]
+        for label, entry in zip(labels, self.register_entries):
+            tk.Label(self.register_window, text=label, bg="#1E2533", fg="#E0E6F0").pack(
+                pady=5
+            )
+            entry.pack(pady=5, padx=10, ipadx=10, ipady=5)
+        register_button = tk.Button(
+            self.register_window,
+            text="Register",
+            command=self.register_user,
+            **self.button_style,
+        )
+        register_button.pack(pady=20, padx=10, ipadx=10, ipady=5)
+
+    def init_login(self):
+        self.login_window = tk.Toplevel(self)
+        self.login_window.title("Login")
+        self.login_window.geometry(f"300x380")
+        self.login_window.configure(bg="#1E2533")
+        header_font = font.Font(family="Helvetica", size=14, weight="bold")
+        tk.Label(
+            self.login_window, text="Login", font=header_font, bg="#1E2533", fg="#E0E6F0"
+        ).pack(pady=20, padx=10, ipadx=10, ipady=5)
+        labels = ["Username:", "Password:"]
+        self.login_entries = [
+            tk.Entry(
+                self.login_window,
+                bg="#46516e",
+                fg="#E0E6F0",
+                show="*" if label == "Password:" else None,
+            )
+            for label in labels
+        ]
+        for label, entry in zip(labels, self.login_entries):
+            tk.Label(self.login_window, text=label, bg="#1E2533", fg="#E0E6F0").pack(pady=5)
+            entry.pack(pady=5, padx=10, ipadx=10, ipady=5)
+        login_button = tk.Button(
+            self.login_window, text="Login", command=self.login_user, **self.button_style
+        )
+        login_button.pack(pady=20, padx=10, ipadx=10, ipady=5)
+        forgot_password_button = tk.Button(
+            self.login_window,
+            text="Forgot Password",
+            command=self.init_forgot_password,
+            **self.button_style,
+        )
+        forgot_password_button.pack(pady=20, padx=10, ipadx=10, ipady=5)
+
+    def init_forgot_password(self):
+        self.forgot_password_window = tk.Toplevel(self)
+        self.forgot_password_window.title("Forgot Password")
+        self.forgot_password_window.geometry(f"300x250")
+        self.forgot_password_window.configure(bg="#1E2533")
+        header_font = font.Font(family="Helvetica", size=14, weight="bold")
+        tk.Label(
+            self.forgot_password_window,
+            text="Forgot Password",
+            font=header_font,
+            bg="#1E2533",
+            fg="#E0E6F0",
+        ).pack(pady=20, padx=10, ipadx=10, ipady=5)
+        tk.Label(
+            self.forgot_password_window, text="Email Address:", bg="#1E2533", fg="#E0E6F0"
+        ).pack(pady=5)
+        self.email_entry = tk.Entry(self.forgot_password_window, bg="#46516e", fg="#E0E6F0")
+        self.email_entry.pack(pady=5, padx=10, ipadx=10, ipady=5)
+        send_email_button = tk.Button(
+            self.forgot_password_window,
+            text="Send Email",
+            command=self.forgot_password,
+            **self.button_style,
+        )
+        send_email_button.pack(pady=20, padx=10, ipadx=10, ipady=5)
+
+    def register_user(self):
+        username = self.register_entries[0].get()
+        email = self.register_entries[1].get()
+        password = self.register_entries[2].get()
+        verify_password = self.register_entries[3].get()
+        if (
+            "" in (username, email, password, verify_password)
+            or password != verify_password
+        ):
+            print("Error: Not all fields have been filled, or the entered password does not match the second password")
+            print("Please try again...")
+            self.register_window.destroy()
+            self.init_register()
+            return
+        keys = ["Username", "Email Address", "Password"]
+        values = [username, email, password]
+        self.send_user_data(0, dict(zip(keys, values)))
+        self.register_window.destroy()
+
+
+    def login_user(self):
+        username = self.login_entries[0].get()
+        password = self.login_entries[1].get()
+        if "" in (username, password):
+            print("Error: Not all fields have been filled, or the entered password does not match the second password")
+            print("Please try again...")
+            self.login_window.destroy()
+            self.init_login()
+            return
+        keys = ["Username", "Password"]
+        values = [username, password]
+        self.send_user_data(1, dict(zip(keys, values)))
+        self.login_window.destroy()
+
+    def forgot_password(self):
+        email = self.email_entry.get()
+        if email == "":
+            print("Error: Not all fields have been filled, or the entered password does not match the second password")
+            print("Please try again...")
+            self.forgot_password_window.destroy()
+            self.init_forgot_password()
+            return
+        self.send_user_data(2, {"Email Address", email})
+        self.forgot_password_window.destroy()
+
+    def send_user_data(
+        self, request_type: int, user_data: dict
+    ) -> None:  # request_type: 0 - register, 1 - login, 2 - forgot password
+        match request_type:
+            case 0:
+                msg = f"|REGS|{user_data['Username']}|{user_data['Email Address']}|{user_data['Password']}"
+            case 1:
+                msg = f"|LOGN|{user_data['Username']}|{user_data['Password']}"
+            case 2:
+                msg = f"|FRGP|{user_data['Email Address']}"
+        send_with_size(self.cli_sock, msg.encode())
+
+    def run(self) -> None:
+        """Run the client application.
+
+        Returns:
+            None
+        """
+        try:
+            self.cli_sock.connect((self.ip, self.port))
+            print(f"Connected to the server {self.ip}:{self.port}")
+            self.init_home()
+        except Exception as e:
+            print(
+                f"Error while trying to connect. Check IP or port -- {self.ip}:{self.port}"
+            )
+            print(f"Error: {e}")
+
+        self.cli_sock.close()
+
+
+if __name__ == "__main__":
+    if len(argv) == 3:
+        ip = argv[1]
+        port = int(argv[2])
+        c = Client(ip, port)
+        c.run()
+    else:
+        #print("Usage: python client.py <ip> <port>")
+        c = Client(IP, PORT)
+        c.run()
--- server.py
+++ server.py
@@ -1,1 +1,66 @@
-import socket
+from socket import socket, AF_INET, SOCK_STREAM, SOL_SOCKET, SO_REUSEADDR
+from threading import Thread
+from sys import argv
+
+IP: str = "0.0.0.0"
+PORT: int = 1234
+
+class Server:
+    def __init__(self, ip: str, port: int) -> None:
+        """Initialize the Server object."""
+        self.threads: list[Thread] = []
+        self.srv_sock: socket = socket(AF_INET, SOCK_STREAM)
+        self.srv_sock.bind((ip, port))
+        self.srv_sock.listen(20)
+        self.srv_sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+
+    @staticmethod
+    def handle_client(cli_sock, id, addr):
+        pass
+
+    def run(self) -> None:
+        """Run the server application.
+
+        Returns:
+            None
+        """
+        i = 1
+        try:
+            print("\nMain thread: starting to accept...")
+            while True:
+                cli_sock, addr = self.srv_sock.accept()
+                t: Thread = Thread(
+                    target=Server.handle_client, args=(cli_sock, str(i), addr)
+                )
+                t.start()
+                i += 1
+                self.threads.append(t)
+                if i > 100000000:
+                    print("\nMain thread: going down for maintenance")
+                    break
+        except KeyboardInterrupt:
+            print("\nMain thread: received keyboard interrupt. Shutting down...")
+        except socket.error as se:
+            print(f"\nMain thread: encountered socket error: {se}")
+        except Exception as e:
+            print(f"\nMain thread: encountered an unexpected error: {e}")
+        finally:
+            print("Main thread: waiting for all clients to die")
+            for t in self.threads:
+                try:
+                    t.join()
+                except Exception as e:
+                    print(f"Error joining thread: {e}")
+            self.srv_sock.close()
+        
+
+if __name__ == "__main__":
+    if len(argv) == 3:
+        ip = argv[1]
+        port = int(argv[2])
+        s = Server(ip, port)
+        s.run()
+    else:
+        #print("Usage: python server.py <ip> <port>")
+        s = Server(IP, PORT)
+        s.run()
